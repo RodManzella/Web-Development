b80bf020cd8d212fdba531afb25ffea1d152bd8b
RodManzella <manzellateixeira@gmail.com>
2023-09-23 09:40:02 -0300

    commit

---
+++ d)Javascript-na-DOM/6)OrdernarArray/script.js
@@ -0,0 +1,14 @@
+let nomes = ["Rodrigo", "Juan", "Vinícius","Janavel", "Birulau", "Macintosh"]
+
+// ordenar um array de forma crescente(altera estrutura do array)
+nomes.sort()
+
+// ordenar um array de forma descendente
+
+nomes.sort()
+nomes.reverse()
+
+//Sintaxe alternativa 
+nomes.sort().reverse()
+
+// também funciona para arrays numéricos, mas cuidado(nçumeros são analisados como strings)
