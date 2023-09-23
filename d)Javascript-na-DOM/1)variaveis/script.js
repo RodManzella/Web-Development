55fc0b465aa138f3133fd80744ee5a439c9af068
RodManzella <manzellateixeira@gmail.com>
2023-09-22 22:52:05 -0300

    COMMIT

---
+++ d)Javascript-na-DOM/1)variaveis/script.js
@@ -0,0 +1,17 @@
+var nome = Rodrigo;
+var idade = 20;
+var estudante = true;
+
+var cidade;
+cidade = "Lisboa";
+
+var valor1 = 100, nome2 ="Ana", apelido = "Silva";
+
+// let e const(constante)
+
+let texto = "frase de um texto";
+const pi = 3.14;
+
+// variáveis declaradas com var tem um escopo de função, enquanto let e const possuem escopo de bloco.
+
+// javascript possui variáveis não tipificadas
---
+++ d)Javascript-na-DOM/2)OperadorTernario/script.js
@@ -0,0 +1,21 @@
+// Operador ternário 
+
+/*
+var resultado = (condição)? valor1 : valor2
+*/
+
+//let enviar_email = false;
+//let frase = enviar_email ? "Enviar": "Não enviar";
+
+//let valor = 10;
+//let frase = valor < 5 ? 'SIM':'NAO';
+
+
+//let valor = 100;
+//valor == 10 ? console.log("SIM"): console.log("NAO");
+
+
+let valor1 = 5;
+let valor2 = 10;
+
+let resposta = valor1 ==5 && valor2 == 10 ? 'OK': 'NOT OK';
