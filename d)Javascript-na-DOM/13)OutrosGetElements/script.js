78be4d215f169e947b40822309d31a9ab5e90b56
RodManzella <manzellateixeira@gmail.com>
2023-09-24 10:47:05 -0300

    commit

--- d)Javascript-na-DOM/13)OutrosGetElements/script.js
+++ d)Javascript-na-DOM/13)OutrosGetElements/script.js
@@ -14,4 +14,26 @@ let e = document.getElementsByClassName("texto-titulo") // retorna array contend
 
 console.log(e[0])
 
-console.log("oi")
+
+// nesse caso temos 3 elementos html que possuem a classe "outra-noticia"
+let a = document.getElementsByClassName("outra-noticia")  // a será um array com 3 elementos
+
+// podemos selecionar um desses 3 elementos html do array(que no casso são hyperlinks)
+// vamos então alterar o texto interno de um deles.
+
+a[0].innerText = "Nova notícia"
+
+
+
+
+let z = document.getElementsByName("text-usuario")  // esse método seleciona elementos que possuam a tag name(geralmente inputs)
+console.log(z) // array de elementos com o nome "text-usuario"
+// no nosso caso é somente um elemento do tipo input que podemos selecionar assim: z[0]
+
+// a partir daí, podemos acessar os atributos desse elemento input:
+z[0].value = "Bitonga"
+
+
+
+
+let x = document.getElementsByTagName("p")  // retorna array com todos elementos p
