907d576b474f266fb33fe3e5b5c28d3a4316b5e0
RodManzella <manzellateixeira@gmail.com>
2023-09-25 18:34:44 -0300

    commit

---
+++ d)Javascript-na-DOM/16)EstilosInlineJS/script.js
@@ -0,0 +1,8 @@
+// Aplicar estilos inline com JavaScript
+
+let el = document.querySelector("h1")  // pega o primeiro h1 que aparecer no documento html e retorna para el
+el.style.backgroundColor = "green"  // muda o background color do h1
+
+
+let el1 = document.querySelector("p")  // pega o primeiro elemento p que aparece no html
+console.log(el.style.backgroundColor) // printa na tela o estilo de background que está aplicado no p (rgb(10,10,10))
