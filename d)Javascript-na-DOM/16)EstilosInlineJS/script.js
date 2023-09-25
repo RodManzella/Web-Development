117c42629727eb78d3cd40d0625f3b2abc49a808
RodManzella <manzellateixeira@gmail.com>
2023-09-25 18:38:04 -0300

    commit

--- d)Javascript-na-DOM/16)EstilosInlineJS/script.js
+++ d)Javascript-na-DOM/16)EstilosInlineJS/script.js
@@ -6,3 +6,9 @@ el.style.backgroundColor = "green"  // muda o background color do h1
 
 let el1 = document.querySelector("p")  // pega o primeiro elemento p que aparece no html
 console.log(el.style.backgroundColor) // printa na tela o estilo de background que está aplicado no p (rgb(10,10,10))
+
+
+
+
+const estilos = window.getComputedStyle(el1)
+console.log(estilos.getPropertyValues('color'))  // pegando informações do CSS de p
